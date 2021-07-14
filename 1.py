397c56aeb9d6af859d52f1976dec686eaa85c408
AndreyLV <office@amtlav.ru>
2021-07-14 07:13:08 +0700

    готовое решение.

--- 1.py
+++ 1.py
@@ -5,25 +5,33 @@
 #from math import factorial
 
 def fun():
-    r = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-    m = [0, 2, 5]
+    tot = 200
+    r =  [[1] * tot,
+          [0] * tot,
+          [0] * tot,
+          [0] * tot,
+          [0] * tot,
+          [0] * tot,
+          [0] * tot,
+          [0] * tot]
+    m = [1, 2, 5, 10, 20, 50, 100, 200]
+    l = len(m)
     res = 0
-    for i in range(1, 3):
-        for j in range(1, 11):
+    for i in range(1, l):
+        for j in range(1, tot+1):
             x = j - m[i]
             if x < 0:
                 continue
             if x == 0:
                 r[i][j-1] = 1
             if x > 0:
-                r[i][j-1] = r[0][x-1] + r[1][x-1] + r[2][x-1]
-
+                for n in range(l):
+                    r[i][j-1] += r[n][x-1]
         #print(r[i])
 
     for i in r:
-        res += i[9]
+        res += i[tot-1]
+
     return res
 
 
